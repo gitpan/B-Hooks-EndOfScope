19887ab01dff88894aca73350eade8366ec643e5
Florian Ragwitz <rafl@debian.org>
2008-10-19 07:32:01 +0000

    Import of FLORA/B-Hooks-EndOfScope-0.01 from CPAN.
    gitpan-cpan-distribution: B-Hooks-EndOfScope
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FLORA/B-Hooks-EndOfScope-0.01.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

---
+++ lib/B/Hooks/EndOfScope.pm
@@ -0,0 +1,68 @@
+use strict;
+use warnings;
+
+package B::Hooks::EndOfScope;
+
+use Scope::Guard;
+
+our $VERSION = '0.01';
+
+our $SCOPE_HOOK_KEY = 'SCOPE_END_HOOK';
+
+use Sub::Exporter -setup => {
+    exports => ['on_scope_end'],
+    groups  => { default => ['on_scope_end'] },
+};
+
+=head1 NAME
+
+B::Hooks::EndOfScope - Execute code after a scope finished compilation
+
+=head1 SYNOPSIS
+
+    on_scope_end { ...  };
+
+=head1 DESCRIPTION
+
+This module allows you to execute code when perl finished compiling the
+surrounding scope.
+
+=head1 FUNCTIONS
+
+=head2 on_scope_end
+
+    on_scope_end { ... };
+
+    on_scope_end $code;
+
+Registers C<$code> to be executed after the surrounding scope has been
+compiled.
+
+=cut
+
+sub on_scope_end (&) {
+    my $cb = shift;
+
+    $^H |= 0x120000;
+    $^H{ $SCOPE_HOOK_KEY } = [Scope::Guard->new($cb), @{ $^H{ $SCOPE_HOOK_KEY } || [] }];
+}
+
+=head1 SEE ALSO
+
+L<Scope::Guard>
+
+=head1 AUTHOR
+
+Florian Ragwitz E<lt>rafl@debian.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2008  Florian Ragwitz
+
+This module is free software.
+
+You may distribute this code under the same terms as Perl itself.
+
+=cut
+
+1;
